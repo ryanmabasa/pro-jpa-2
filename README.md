930874f0614e62024e8c57d65909b07ce8274bcd
Ryan Mabasa <ryanmabasa07@gmail.com>
2022-12-20 13:07:59 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+
+# Pro Jpa 2
+
+Pro jpa maven project from scratch
+## Installation
+
+1. Create database file in home folder
+2. File format should be *databasename*.mv.db
+3. This is the sample url to access jdbc:h2:tcp://localhost/~/testdb
+4. Run the jar file
+
+## Additional Information
+
+http://www.h2database.com/html/cheatSheet.html
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class Main {
+    public static void main(String[] args) {
+        Employee employee = new Employee();
+        employee.setId(1L);
+        employee.setName("Foo");
+
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("myApp");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
+        transaction.begin();
+        entityManager.persist(employee);
+        transaction.commit();
+        entityManager.close();
+//        tables will be deleted
+//        entityManagerFactory.close();
+
+
+    }
+}
